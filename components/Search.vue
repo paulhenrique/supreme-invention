7fc22f52aa3f51a53c2f1b4f31f56ede51f89e50
Paulo Henrique <phvcandido@gmail.com>
2022-02-12 15:03:27 -0300

    Adicionando instantsearch e configurações para nuxt

---
+++ components/Search.vue
@@ -0,0 +1,69 @@
+<template>
+  <ais-instant-search index-name="demo_ecommerce" :search-client="searchClient">
+    <div class="left-panel">
+      <ais-clear-refinements />
+      <h2>Brands</h2>
+      <ais-refinement-list attribute="brand" searchable />
+      <ais-configure :hitsPerPage="8" />
+    </div>
+    <div class="right-panel">
+      <ais-search-box />
+      <ais-hits>
+        <template v-slot:item="{ item }">
+          <h2>{{ item.name }}</h2>
+          <img :src="item.image" align="left" :alt="item.name" />
+          <div class="hit-name">
+            <ais-highlight attribute="name" :hit="item"></ais-highlight>
+          </div>
+          <div class="hit-description">
+            <ais-highlight attribute="description" :hit="item"></ais-highlight>
+          </div>
+          <div class="hit-price">{{ item.price }}</div>
+        </template>
+      </ais-hits>
+      <ais-pagination />
+    </div>
+  </ais-instant-search>
+</template>
+
+<script>
+import algoliasearch from 'algoliasearch/lite'
+import 'instantsearch.css/themes/satellite-min.css'
+export default {
+  data() {
+    return {
+      searchClient: algoliasearch(
+        'B1G2GM9NG0',
+        'aadef574be1f9252bb48d4ea09b5cfe5'
+      ),
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  padding: 1em;
+}
+.ais-Hits-list {
+  margin-top: 0;
+  margin-bottom: 1em;
+}
+.ais-InstantSearch {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 1em;
+}
+.ais-Hits-item img {
+  margin-right: 1em;
+}
+.hit-name {
+  margin-bottom: 0.5em;
+}
+.hit-description {
+  color: #888;
+  font-size: 0.8em;
+  margin-bottom: 0.5em;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,19 +15,42 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/vue-instantsearch.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ['nuxt-content-algolia'],
+  nuxtContentAlgolia: {
+    // Application ID
+    appId: '34IIDW6KKR',
 
+    // Admin API Key
+    // !IMPORTANT secret key should always be an environment variable
+    // this is not your search only key but the key that grants access to modify the index
+    apiKey: '3f8d80be6c42bb030d27a7f108eb75f8',
+
+    paths: [
+      {
+        name: 'articles',
+        index: process.env.ALGOLIA_INDEX || 'articles',
+        fields: ['title', 'description', 'tags', 'bodyPlainText'],
+      },
+    ],
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    'nuxt-algolia',
   ],
+  publicRuntimeConfig: {
+    algolia: {
+      applicationId: 'B1G2GM9NG0',
+      apiKey: 'aadef574be1f9252bb48d4ea09b5cfe5',
+    },
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -37,5 +60,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['vue-instantsearch', 'instantsearch.js/es'],
+  },
 }
